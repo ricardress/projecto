467a296d94eadc34fe3c376a81127156e2a1cf60
ricardoAnSB <ricardosab016@gmail.com>
2023-01-12 21:37:00 -0500

    tird Commit

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    apellido = models.CharField(max_length=50, blank=False, null=False)
+    telefono = models.CharField(max_length=12, blank=False, null=False)
+    correo = models.EmailField(blank=False, null=False)
+    fecha = models.DateField(default=date.today)
+    notas = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.nombre
--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,73 @@
-from django.shortcuts import render
+from asyncio.windows_events import NULL
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from .models import Usuario
+from .forms import UsuarioForm
 
 
-def index(request):
+def index(request, letter=NULL):
+    if letter != NULL:
+        usuarios = Usuario.objects.filter(nombre__istartswith=letter)
+    else:
+        usuarios = Usuario.objects.filter(nombre__contains=request.GET.get('buscar', ''))
 
-    return render(request, 'index.html')
+    context = {
+        'usuarios': usuarios
+    }
+
+    return render(request, 'usuario/index.html', context)
+
+
+def view(request, id):
+    usuarios = Usuario.objects.get(id=id)
+
+    context = {
+        'usuarios': usuarios
+    }
+
+    return render(request, 'usuario/detail.html', context)
+
+
+def edit(request, id):
+    usuarios = Usuario.objects.get(id=id)
+
+    if request.method == "GET":
+        form = ContactoForm(instance=usuarios)
+        context = {
+            'form': form,
+            'id': id
+        }
+        return render(request, 'usuario/edit.html', context)
+
+    if request.method == "POST":
+        form = UsuarioForm(request.POST, instance=usuarios)
+        if form.is_valid():
+            form.save()
+        context = {
+            'form': form,
+            'id': id
+        }
+        messages.success(request, 'Contacto actualizado')
+        return render(request, 'usuario/edit.html', context)
+
+
+def create(request):
+    if request.method == "GET":
+        form = UsuarioForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'usuario/create.html', context)
+
+    if request.method == "POST":
+        form = UsuarioForm(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect('usuario')
+
+
+def delete(request, id):
+    usuarios = Usuario.objects.get(id=id)
+    usuarios.delete()
+    return redirect('usuario')
+
