0c70cfb0ab61da7319a19f8c360d085df0f1777a
ricardoAnSB <ricardosab016@gmail.com>
2023-01-13 21:24:04 -0500

    10th Commit

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='usuario'),
+    #path('', views.index, name='usuario'),
     #path('<letter>', views.index, name='usuario'),
     #path('view/<int:id>', views.view, name='usuario_view'),
     #path('edit/<int:id>', views.edit, name='usuario_edit'),
