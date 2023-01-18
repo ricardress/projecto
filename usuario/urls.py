1169d9d70112e597401943b49db7badbc68f7e53
ricardoAnSB <ricardosab016@gmail.com>
2023-01-18 09:57:40 -0500

    18th Commit

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='usuario'),
     path('<letter>', views.index, name='usuario'),
-    #path('view/<int:id>', views.view, name='usuario_view'),
-    #path('edit/<int:id>', views.edit, name='usuario_edit'),
-    #path('create/', views.create, name='usuario_create'),
-    #path('delete/<int:id>', views.delete, name='usuario_delete')
+    path('view/<int:id>', views.view, name='usuario_view'),
+    path('edit/<int:id>', views.edit, name='usuario_edit'),
+    path('create/', views.create, name='usuario_create'),
+    path('delete/<int:id>', views.delete, name='usuario_delete')
 ]
